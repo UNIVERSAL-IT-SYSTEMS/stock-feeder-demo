4774d3e7815b167f9a37743bd956959297f37993
André Dumas <agdumas@gmail.com>
2012-10-22 00:54:38 +1100

    Renamed install script, made executable

--- install-web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PWD=`pwd`
-SCRIPT_DIR=`dirname $0`
-cd $SCRIPT_DIR
-git pull
-git reset HEAD --hard
-git submodule update --init
-puppet apply --modulepath=$SCRIPT_DIR/puppet/modules $SCRIPT_DIR/puppet/manifests/site.pp
