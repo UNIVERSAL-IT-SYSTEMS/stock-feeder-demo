c4c3524edf5d6403efcb8eac495895587ac10103
André Dumas <agdumas@gmail.com>
2012-10-25 02:22:20 +1100

    Initial rich client outline

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,30 +1,59 @@
 package controllers
 
-import scala.collection.mutable
 import java.net.URL
+import scala.collection.mutable
+import scala.io.Source
+import akka.util.duration.intToDurationInt
+import play.api.Logger
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms.boolean
+import play.api.data.Forms.nonEmptyText
+import play.api.data.Forms.tuple
+import play.api.libs.concurrent.Akka
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Enumerator.Pushee
 import play.api.mvc.Action
 import play.api.mvc.Controller
-import scala.io.Source
-import play.api.Logger
-import play.api.data._
-import play.api.data.Forms._
-import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
+import play.api.libs.json.JsObject
+import play.api.libs.json.Format
+import play.api.libs.json.JsString
+import play.api.libs.json.JsNumber
+import scala.collection.immutable.TreeSet
 
-case class Stock(id: Int, fullSymbol: String, price: java.lang.Double, direction: String) {
+object Stock {
+  def apply(id: Int, fullSymbol: String) = {
+    new Stock(id, fullSymbol, 0D, "/")
+  }
+}
+case class Stock(id: Int, fullSymbol: String, price: java.lang.Double, direction: String) extends Ordered[Stock] {
   val symbol = if(fullSymbol.indexOf('.') >= 0) {
 	  fullSymbol.take(fullSymbol.indexOf('.'))
   }
   else {
     fullSymbol
   }
+  def compare(other:Stock) = symbol compare other.symbol
 }
+
+case class StockPushee(pushee:Pushee[String], var count: Int = 0)
 
 object Application extends Controller {
   val yahooRegex = """([\^\w\.]*)",([\d\.]*),"([\d\/]*)","([\dapm:]*)",([\+-\.\d]*),([\d\.]*),([\d\.]*),([\d\.]*),([\d]*).*""".r
    
-  var update = false
-  val stockList = mutable.Map[String, Stock]()
-      
+  var update = true
+  val stockList = mutable.Map[String, Stock]("AEX.AS" -> Stock(0, "AEX.AS"), "FUR.AS" -> Stock(1, "FUR.AS"))
+  val stockPushees = new mutable.HashSet[StockPushee]() with mutable.SynchronizedSet[StockPushee]
+
+  Akka.system.scheduler.schedule(0 seconds, 2 seconds) {
+    if(!stockPushees.isEmpty) {
+    	Logger.info("Processing pushees...")
+    	processPushees
+    }
+  }
+  
   val stockListForm = Form(
 		  tuple("stockList" -> nonEmptyText,
 		      "update" -> boolean)
@@ -95,26 +124,69 @@ object Application extends Controller {
   def pollStream = Action {
 	  val dataStream = Enumerator.pushee[String] (
 		  	onStart = { pushee =>
-		  	  // Push everything the first time a client connects
-		  	  pushee.push(views.html.poll(stockList.values.iterator).toString.trim);
+		  	  // Add to the list to process
+		  	  stockPushees.add(new StockPushee(pushee))
 		  	  
-		  	  // Stream data for 2 minutes
-		  	  for(i <- 0 until 60) {
-		    	val quotes = getQuotes
-	  	    	// If there are quotes
-	  	    	if(!quotes.isEmpty) {
-	  	    	  // Push them out
-	  	    	  pushee.push(views.html.poll(quotes).toString.trim);
-	  	    	}
-	  	    	
-	  		  	// Semi 'realtime'
-	  		  	Thread.sleep(2000)
-		  	  }
-
-		  	  pushee.close; 
+		  	  // Push everything the first time a client connects
+		  	  // TODO Sort the list
+		  	  pushee.push(views.html.poll(stockList.valuesIterator).toString.trim)
+		  	},
+		  	onComplete = { Logger.info("Complete") },
+		  	onError = { (error, input) => 
+		  	  Logger.error("Error with stream: " + error + " input: " + input) 
 		  	}
 	  )
 
 	  Ok.stream(dataStream)
+  }
+  
+  	// if you want to directly serialize/deserialize, you need to write yourself a formatter right now
+	implicit object StockFormat extends Format[Stock] {
+	    def reads(json: JsValue): Stock = Stock((json \ "id").as[Int], (json \ "symbol").as[String])
+	    def writes(stock: Stock): JsValue = JsObject(
+	        List("id" -> JsNumber(stock.id),
+	        		"symbol" -> JsString(stock.symbol),
+	        		"price" -> JsNumber(new java.math.BigDecimal(stock.price.toString)),
+	        		"direction" -> JsString(stock.direction)
+	        	)
+	    )
+	
+	}
+
+  def pollStreamJson = Action {
+    // TODO Sort the list
+    Ok(Json.toJson(stockList.values.toSeq))
+  }
+  
+  def processPushees = {
+    val quotes = getQuotes
+	// If there are quotes
+	if(!quotes.isEmpty) {
+		// Push them out for each pushee
+		stockPushees.foreach { stockPushee => 
+	    	stockPushee.pushee.push(views.html.poll(quotes).toString.trim) 
+	    	Logger.info("Processed pushee: " + stockPushee)
+		}
+		
+		// TODO Send out the Json promises
+		// jsonPromises...
+	}
+    
+    // Close the ones that have expired
+    // TODO Figure out why this retain is not working
+    stockPushees.retain { stockPushee => 
+	    stockPushee.count += 1
+	    
+	    if(stockPushee.count > 3) {
+	    	Logger.info("Removing pushee: " + stockPushee)
+	    	stockPushee.pushee.close
+	    }
+	    
+      	stockPushee.count <= 3
+    }
+  }
+  
+  def view = Action {
+    Ok(views.html.view())
   }
 }
